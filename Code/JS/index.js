fa09fe54da36a75fe84cf87d53d8cfea5d05ef38
JasperRutten1 <60922940+JasperRutten1@users.noreply.github.com>
2020-12-17 19:56:56 +0100

    contact pagina

--- Code/JS/index.js
+++ Code/JS/index.js
@@ -2,10 +2,9 @@ function achtergrondGroote(){
     let pic = document.getElementById("achtergrond-afbeelding");
 
     let offSetY = pic.offsetTop;
-    let headerY = document.getElementsByTagName("header").item(0).offsetTop;
     let totalY = screen.availHeight;
 
-    pic.style.height = totalY - offSetY - 100 + "px";
+    pic.style.height = Math.max(totalY - offSetY - 100, 400) + "px";
 }
 
 achtergrondGroote();
