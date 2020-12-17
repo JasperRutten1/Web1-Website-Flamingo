ffac54bf1b15ab841f6d3638ccd7e5f9a590bf82
JasperRutten1 <60922940+JasperRutten1@users.noreply.github.com>
2020-12-18 00:36:58 +0100

    tekst and css finishing touches

--- Code/JS/index.js
+++ Code/JS/index.js
@@ -1,3 +1,11 @@
+achtergrondGroote();
+welkomBerichtHoogte();
+
+window.onresize = () => {
+    achtergrondGroote();
+    welkomBerichtHoogte();
+}
+
 function achtergrondGroote(){
     let pic = document.getElementById("achtergrond-afbeelding");
 
@@ -7,8 +15,12 @@ function achtergrondGroote(){
     pic.style.height = Math.max(totalY - offSetY - 100, 400) + "px";
 }
 
-achtergrondGroote();
+function welkomBerichtHoogte(){
+    let pic = document.getElementById("achtergrond-afbeelding");
+    let titel = pic.children.item(0);
 
-window.onresize = () => {
-    achtergrondGroote();
+    let top = pic.offsetTop;
+    let bot = pic.offsetHeight;
+
+    titel.style.top = ((bot/2) + top) + "px";
 }
--- Code/JS/reeks.js
+++ Code/JS/reeks.js
@@ -7,12 +7,29 @@ for(let i = 0 ; i < products.length ; i++){
         let showing = document.getElementById("showing-info");
         if(showing != null){
             showing.removeAttribute('id');
+            let dummy = document.getElementById("dummy");
+            dummy.remove();
         }
-        let children = prod.children;
-        if(!(showing === children.item(1))){
-            children.item(1).setAttribute('id', "showing-info")
+        if(!(showing === prod)){
+            let dummy = prod.cloneNode(true);
+            dummy.setAttribute('id', "dummy");
+            prod.parentNode.insertBefore(dummy, prod);
+
+            prod.setAttribute('id', "showing-info")
+            prod.style.top = dummy.offsetTop - 10 + "px";
+            prod.style.left = dummy.offsetLeft - 8 + "px";
+
+            console.log(dummy.width)
         }
     })
+}
+
+window.onresize = () => {
+    let dummy = document.getElementById("dummy");
+    if(dummy == null) return;
+    let showing = document.getElementById("showing-info");
+    showing.style.top = dummy.offsetTop - 10 + "px";
+    showing.style.left = dummy.offsetLeft - 8 + "px";
 }
 
 
